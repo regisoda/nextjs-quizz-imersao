31a5e97d618436ac200052bdc2a0e7579f1e477d
Regis Oda <regis@otk.com.br>
2021-01-29 14:41:36 -0300

    Final aula 04

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,14 +1,49 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-tabs */
 import Head from 'next/head';
 import React from 'react';
 
 import db from '../db.json';
+import AlternativesForm from '../src/components/AlternativesForm';
+import Button from '../src/components/Button';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
-import Button from '../src/components/Button';
+
+function ResultWidget({ results }) {
+  return (
+    <Widget>
+      <Widget.Header>
+        Tela de Resultado:
+      </Widget.Header>
+
+      <Widget.Content>
+        <p>
+          Você acertou
+          {' '}
+          {results.filter((x) => x).length}
+          {' '}
+          perguntas
+        </p>
+        <ul>
+          {results.map((result, index) => (
+            <li key={`result__${result}`}>
+              #
+              {index + 1}
+              {' '}
+              Resultado:
+              {result === true
+                ? ' Acertou'
+                : ' Errou'}
+            </li>
+          ))}
+        </ul>
+      </Widget.Content>
+    </Widget>
+  );
+}
 
 function LoadingWidget() {
   return (
@@ -29,8 +64,15 @@ function QuestionWidget({
   questionIndex,
   totalQuestions,
   onSubmit,
+  addResult,
 }) {
+  const [selectedAlternative, setSelectedAlternative] = React.useState(undefined);
+  const [isQuestionSubmited, setIsQuestionSubmited] = React.useState(false);
   const questionId = `question__${questionIndex}`;
+  const isCorrect = selectedAlternative === question.answer;
+  const hasAlternativeSelected = selectedAlternative !== undefined;
+
+
   return (
     <Widget>
       <Widget.Header>
@@ -57,24 +99,35 @@ function QuestionWidget({
           {question.description}
         </p>
 
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            onSubmit();
+        <AlternativesForm
+          onSubmit={(infosDoEvento) => {
+            infosDoEvento.preventDefault();
+            setIsQuestionSubmited(true);
+            setTimeout(() => {
+              addResult(isCorrect);
+              onSubmit();
+              setIsQuestionSubmited(false);
+              setSelectedAlternative(undefined);
+            }, 3 * 1000);
           }}
         >
           {question.alternatives.map((alternative, alternativeIndex) => {
             const alternativeId = `alternative__${alternativeIndex}`;
+            const alternativeStatus = isCorrect ? 'SUCCESS' : 'ERROR';
+            const isSelected = selectedAlternative === alternativeIndex;
             return (
               <Widget.Topic
                 as="label"
-                key={alternativeIndex}
+                key={alternativeId}
                 htmlFor={alternativeId}
+                data-selected={isSelected}
+                data-status={isQuestionSubmited && alternativeStatus}
               >
                 <input
-                  // style={{ display: 'none' }}
+                  style={{ display: 'none' }}
                   id={alternativeId}
                   name={questionId}
+                  onChange={() => setSelectedAlternative(alternativeIndex)}
                   type="radio"
                 />
                 {alternative}
@@ -83,13 +136,17 @@ function QuestionWidget({
           })}
 
           {/* <pre>
-            {JSON.stringify(question, null, 4)}
-          </pre> */}
+             {JSON.stringify(question, null, 4)}
+           </pre> */}
 
-          <Button type="submit">
+          <Button type="submit" disabled={!hasAlternativeSelected}>
             Confirmar
           </Button>
-        </form>
+          {isQuestionSubmited && isCorrect && <p>Você acertou!</p>}
+          {isQuestionSubmited && !isCorrect && <p>Você errou!</p>}
+
+        </AlternativesForm>
+
       </Widget.Content>
     </Widget>
   );
@@ -105,10 +162,19 @@ export default function QuizPage() {
   // console.log(`QUESTIONS =>`, db.questions);
 
   const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const [results, setResults] = React.useState([]);
   const totalQuestions = db.questions.length;
   const [currentQuestion, setCurrentQuestion] = React.useState(0);
   const questionIndex = currentQuestion;
   const question = db.questions[questionIndex];
+
+  function addResult(result) {
+    // results.push(result);
+    setResults([
+      ...results,
+      result,
+    ]);
+  }
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -139,12 +205,13 @@ export default function QuizPage() {
             questionIndex={questionIndex}
             totalQuestions={totalQuestions}
             onSubmit={handleSubmitQuiz}
+            addResult={addResult}
           />
         )}
 
         {screenState === screenStates.LOADING && <LoadingWidget />}
 
-        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+        {screenState === screenStates.RESULT && <ResultWidget results={results} />}
 
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/regisoda/nextjs-quizz-imersao" />
@@ -152,35 +219,3 @@ export default function QuizPage() {
   )
 }
 
-// function Quiz() {
-//   // console.log(db.questions);
-
-//   return (
-//     <QuizBackground backgroundImage={db.bg}>
-//       <Head>
-//         <title>Quiz - Xadrez</title>
-//       </Head>
-//       <QuizContainer>
-//         <QuizLogo />
-//         <Widget>
-//           <Widget.Header>
-//             <h1>{db.title}</h1>
-//           </Widget.Header>
-//           <Widget.Content>Em construção</Widget.Content>
-//         </Widget>
-
-//         <Widget>
-//           <Widget.Content>
-//             <h1>Quizes da galera</h1>
-//           </Widget.Content>
-//         </Widget>
-//         <Footer />
-
-//         <LoadingWidget />
-//       </QuizContainer>
-//       <GitHubCorner projectUrl="https://github.com/regisoda/nextjs-quizz-imersao" />
-//     </QuizBackground>
-//   );
-// }
-
-// export default Quiz;
